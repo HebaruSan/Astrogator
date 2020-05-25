2d0f58483e215e62b6ddccfd6bb70eebb0c2229f
Paul Hebble <pjhebble@gmail.com>
2020-05-25 12:24:21 -0500

    Update version to 0.10.1

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HebaruSan")]
 [assembly: AssemblyProduct("Astrogator")]
-[assembly: AssemblyCopyright("Copyright © 2017-2019")]
+[assembly: AssemblyCopyright("Copyright © 2017-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyVersion("0.10.1.0")]
+[assembly: AssemblyFileVersion("0.10.1.0")]
